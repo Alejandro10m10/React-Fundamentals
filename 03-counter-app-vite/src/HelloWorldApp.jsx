d74acf18fc63d07d5c5e66466e28b89b18ca4a44
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-06-20 20:40:17 -0500

    Add function to prin variables in the HTML

--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -1,8 +1,13 @@
 
+const getResult = (a, b) => {
+    return a + b;
+}
+
 export const HelloWorldApp = () => {
+
     return (
         <>
-            <h1>Hello World App</h1>
+            <h1>Hello { getResult(1, 2) }</h1>
             <p>Soy un subtitulo</p>
         </>
     );
